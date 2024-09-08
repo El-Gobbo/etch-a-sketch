5f76b36cfd60074f9d730ad9e7ba026aef4964e0
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-08 22:27:46 +0100

    Put row and cell generation into functions
    This was done to aid readability.

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 const cont = document.querySelector("#container");
 
-
-
 let rowDiv;
 let pixelDiv;
-let windowHeight = window.innerHeight;
-let pixelHeight = windowHeight/16;
-let windowWidth = window.innerWidth;
-let pixelWidth = windowWidth/16;
 
-for (let i = 0; i < 16;i++){
+function createRow (rowNumber){
     rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
-    rowDiv.setAttribute(`id`,`row${i}`);
-    rowDiv.style.width = windowWidth;
-    rowDiv.style.height = pixelHeight;
+    rowDiv.setAttribute(`id`,`row${rowNumber}`);
     cont.appendChild(rowDiv);
+}
+
+function createCell(rowNumber, cellNumber){
+    pixelDiv = document.createElement("div");
+    pixelDiv.classList.add("pixel");
+    pixelDiv.setAttribute(`id`,`pixel${rowNumber}-${cellNumber}`);
+    document.querySelector(`#row${rowNumber}`).appendChild(pixelDiv);
+}
+
+for (let i = 0; i < 16;i++){
+    createRow(i);
     for (let j = 0; j < 16; j++){
-        pixelDiv = document.createElement("div");
-        pixelDiv.classList.add("pixel");
-        pixelDiv.style.width = pixelWidth;
-        pixelDiv.style.height = pixelHeight;
-        pixelDiv.setAttribute(`id`,`pixel${i}-${j}`);
-        document.querySelector(`#row${i}`).appendChild(pixelDiv);
+        createCell(i,j);
     }
 }
