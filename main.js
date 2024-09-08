4523485f16e73a6e83de882ac9e1c978ffd30232
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-08 23:53:15 +0100

    Console.log removed

--- main.js
+++ main.js
@@ -10,12 +10,10 @@ function resizeGrid(){
     let promptMessage = `What dimensions do you want your grid to be (X by X)? Enter a number between 1 and 100.`;
     let newSize = prompt(promptMessage);
     newSize = Number(newSize);
-    console.log(newSize)
     while (isNaN(newSize)
         || newSize < 1
         || newSize > 100
         || newSize != Math.round(newSize)){
-        console.log(newSize)
         if (newSize === null) return;
         alert(`Invalid input`);
         newSize = prompt(promptMessage);
