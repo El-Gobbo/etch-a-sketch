c8f7524c25a7bd975b4c72b050778aa389ef211a
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-08 23:42:14 +0100

    Fix bug resulting from previous commit

--- main.js
+++ main.js
@@ -8,14 +8,19 @@ button.addEventListener(`click`,resizeGrid);
 
 function resizeGrid(){
     let promptMessage = `What dimensions do you want your grid to be (X by X)? Enter a number between 1 and 100.`;
-    let newSize = prompt(promptMessage)
-    while (typeof(newSize) !== 'number'
+    let newSize = prompt(promptMessage);
+    newSize = Number(newSize);
+    console.log(newSize)
+    while (isNaN(newSize)
         || newSize < 1
-        || newSize !== Math.round(newSize)){
+        || newSize > 100
+        || newSize != Math.round(newSize)){
+        console.log(newSize)
+        if (newSize === null) return;
         alert(`Invalid input`);
         newSize = prompt(promptMessage);
     }
-    
+
 }
 
 function createRow (rowNumber){
