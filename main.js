b885d43fee6673423d04c79d67dcdcb8144b0a11
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-08 22:20:01 +0100

    Set container to fill the entire window

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ const cont = document.querySelector("#container");
 
 let rowDiv;
 let pixelDiv;
+let windowHeight = window.innerHeight;
+let pixelHeight = windowHeight/16;
+let windowWidth = window.innerWidth;
+let pixelWidth = windowWidth/16;
 
 for (let i = 0; i < 16;i++){
     rowDiv = document.createElement("div");
-    rowDiv.style.border = "2px solid black";
     rowDiv.classList.add("row");
     rowDiv.setAttribute(`id`,`row${i}`);
+    rowDiv.style.width = windowWidth;
+    rowDiv.style.height = pixelHeight;
     cont.appendChild(rowDiv);
     for (let j = 0; j < 16; j++){
         pixelDiv = document.createElement("div");
         pixelDiv.classList.add("pixel");
+        pixelDiv.style.width = pixelWidth;
+        pixelDiv.style.height = pixelHeight;
         pixelDiv.setAttribute(`id`,`pixel${i}-${j}`);
         document.querySelector(`#row${i}`).appendChild(pixelDiv);
     }
